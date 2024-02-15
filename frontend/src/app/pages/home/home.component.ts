c6f6a8231762ea046bc7aa56671f29025327f873
Gabriella Koos <koos.gabriella92@gmail.com>
2024-02-15 15:52:51 +0100

    add home and header components

---
+++ frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { News } from '../../models/news';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent implements OnInit {
+  news: News[] = [];
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService
+      .getNews()
+      .subscribe((newsList) => (this.news = newsList.news));
+  }
+}
