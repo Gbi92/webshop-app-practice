3ac55f4697d50f65284775972093b95ecba0f35d
Gabriella Koos <koos.gabriella92@gmail.com>
2024-02-16 17:03:34 +0100

    add routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,9 +7,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NewsCardComponent } from './components/news-card/news-card.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent, NewsCardComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NewsCardComponent,
+    RegistrationComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
