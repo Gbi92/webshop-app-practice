e0a9fa7d714425a7324f0340f545867553a9fe20
Gabriella Koos <koos.gabriella92@gmail.com>
2024-04-18 11:58:08 +0200

    correct order model sql

--- backend/src/models/order.js
+++ backend/src/models/order.js
@@ -3,39 +3,35 @@ import { v4 as uuid } from 'uuid';
 
 export const orderModel = {
   async insertOrderData(userId, shippingDetails, orderPrice, shippingPrice) {
-    const { zip, city, street, countryId, lastName, phoneNumber} = shippingDetails;
+    const { zip, city, street, countryId, lastName, phoneNumber, firstName, additionalAddress} = shippingDetails;
     const orderId = uuid();
-    let insertedValues = [orderId, userId, orderPrice, shippingPrice, zip, city, street, countryId, lastName, phoneNumber];
+    const SQLinsertQuery = `
+      INSERT INTO \`order\` 
+        (id, user_id, order_price, shipping_price, zip_code, city, street, country_id, last_name, phone_number, first_name, additional_address)
+        VALUES (?,?,?,?,?,?,?,?,?,?,?,?);
+    `;
+    let insertedValues = [orderId, userId, orderPrice, shippingPrice, zip, city, street, countryId, lastName, phoneNumber, firstName, additionalAddress];
 
-    if (shippingDetails.firstName !== undefined) {
-      insertedValues.push(shippingDetails.firstName);
-    }
-    if (shippingDetails.additionalAddress !== undefined) {
-      insertedValues.push(shippingDetails.additionalAddress);
-    }
+    await db.query(SQLinsertQuery, insertedValues);
 
-    await db.query(
-      SQLinsert(shippingDetails),
-      insertedValues
-    );
-
-    const insertedOrder = await db.query('SELECT * FROM `order` WHERE id=?', [orderId]);
+    const insertedOrder = await db.query('SELECT * FROM `order` WHERE id=?;', [orderId]);
 
     return insertedOrder.results[0];
   },
 
   async insertOrderItemData(orderId, cartItem) {
     const insertOrderItemResult = await db.query(
-      'INSERT INTO orderItem (order_id, product_id, quantity) VALUES (?,?,?)',
+      'INSERT INTO orderItem (order_id, product_id, quantity) VALUES (?,?,?);',
       [orderId, cartItem.product_id, cartItem.quantity]
     );
     return insertOrderItemResult.results.insertId;
   },
 
   async selectOrderData(orderId) {
+    // TODO: ez igy jo?
     let orderData = {};
     const orderDetails = await db.query(
-      `SELECT o.id, o.zip_code, o.city, o.street, c.name AS country, o.additional_address, o.phone_number, o.last_name, o.first_name, o.order_price, o.shipping_price
+      `SELECT o.*, c.name AS country
         FROM \`order\` AS o
         INNER JOIN country AS c ON o.country_id=c.id
         WHERE o.id=?;`,
@@ -52,23 +48,4 @@ export const orderModel = {
     orderData.orderItems = orderItem.results;
     return orderData;
   }
-}
-
-function SQLinsert(shippingDetails) {
-  let SQLinsertQuery = 'INSERT INTO `order` (id, user_id, order_price, shipping_price, zip_code, city, street, country_id, last_name, phone_number';
-  if (shippingDetails.firstName !== undefined) {
-    SQLinsertQuery += ', first_name';
-  }
-
-  if (shippingDetails.additionalAddress !== undefined) {
-    SQLinsertQuery += ', additional_address';
-  }
-
-  SQLinsertQuery += ') VALUES (';
-
-  for (let i = 0; i < Object.keys(shippingDetails).length; i++) {
-    SQLinsertQuery += '?,';
-  }
-
-  return SQLinsertQuery += '?,?,?,?)';
 }
